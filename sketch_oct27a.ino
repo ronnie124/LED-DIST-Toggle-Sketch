3e2970e87a0f43d219f407e78a81b855a1274fb6
ronnie124 <ronniemorris65@gmail.com>
2020-10-27 19:39:19 -0700

    Update sketch_oct27a.ino

--- sketch_oct27a.ino
+++ sketch_oct27a.ino
@@ -5,7 +5,7 @@
 // defines variables
 long duration; 
 int distance; 
-int toggle = 0;
+int toggle;
 
 
 void setup() {
@@ -13,6 +13,7 @@ void setup() {
   pinMode(trigPin, OUTPUT); // 
   pinMode(echoPin, INPUT); // 
   Serial.begin(9600); // // 
+  toggle = 0;
 }
 void loop() {
   Serial.println("Loop started over");
@@ -24,17 +25,21 @@ void loop() {
   distance = duration * 0.034 / 2; 
   Serial.print("Distance: ");
   Serial.println(distance);
+  Serial.println(toggle);
   if (distance < 20) {
     Serial.println("We have received a toggle notification");
     toggle = !toggle;
     Serial.println(toggle); // Will return a 0 and a 1 upon going under 20 cm
-    if (toggle = 0) { // This is the part of the code that won't work for some damn reason
+    if (toggle == 0) { // This is the part of the code that won't work for some damn reason
       digitalWrite(LEDPin, LOW);
-    if (toggle = 1) {
+      Serial.println("It's off");
+    if (toggle == 1) {
+      Serial.println("It's on");
       digitalWrite(LEDPin, HIGH);
     }
     } // End part of code that won't work
   }
   
   delay(1500);
+}
 }
